d95964473fe7b686b6d096d1f45c411c509a4455
KrittaponP <krittapon.phacharoen@gmail.com>
2025-01-03 00:18:12 +0700

    Updated

--- CanCommunication/CanCommunication.h
+++ CanCommunication/CanCommunication.h
@@ -2,7 +2,7 @@
 #define CAN_COMMUNICATION_H
 
 #include "driver/gpio.h"
-#include "driver/can.h"
+#include "driver/twai.h"
 
 class CanCommunication
 {
@@ -10,8 +10,8 @@ public:
     CanCommunication(gpio_num_t tx_io, gpio_num_t rx_io, int baudrate = 500000);
     bool begin();
     bool start();
-    bool transmit(uint32_t identifier, uint8_t *data, size_t len);
-    bool receive(can_message_t *message);
+    bool transmit(uint32_t identifier, uint8_t *data, size_t len, bool isExtended);
+    bool receive(twai_message_t *message, uint32_t waiting_time);
 
 private:
     gpio_num_t tx_io;
